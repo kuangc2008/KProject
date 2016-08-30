6596a95c134b3852727b16c95962889415b84cd6
kuangcheng <kuangcheng01@baidu.com>
2016-08-30 17:41:53 +0800

    排序

--- 50_android_hacks/src/androidTest/java/com/kc/a50_android_hacks/ApplicationTest.java
+++ 50_android_hacks/src/androidTest/java/com/kc/a50_android_hacks/ApplicationTest.java
@@ -1,11 +1,20 @@
 package com.kc.a50_android_hacks;
 
 import android.app.Application;
+import android.net.Uri;
+import android.os.Build;
 import android.test.ApplicationTestCase;
 import android.text.Html;
 import android.text.SpannableString;
 import android.text.TextUtils;
 import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
@@ -43,12 +52,52 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
         //如果有&#25105; 会转成汉字
         String decodeA3 =  Html.fromHtml(a).toString();    //将特殊符号，转成<
 
-        Log.i("kcc", "-------------------");
-        Log.i("kcc", encodeA2);
-        Log.i("kcc", decodeA2);
-        Log.i("kcc", "-------------------");
-        Log.i("kcc", decodeA3);
 
+
+//        Log.i("kcc", "-------------------");
+//        Log.i("kcc", encodeA2);
+//        Log.i("kcc", decodeA2);
+//        Log.i("kcc", "-------------------");
+//        Log.i("kcc", decodeA3);
+
+
+        JSONObject b = new JSONObject();
+        try {
+            b.put("aaa", "'");
+            b.put("bbb", "\"");
+            b.put("ccc", "<");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        String b2 = TextUtils.htmlEncode(b.toString());
+        String b3 =  Html.fromHtml(b2).toString();    //将特殊符号，转成<
+
+        Log.i("kcc", b.toString());
+        Log.i("kcc", b2);
+        Log.i("kcc", b3);
+
+
+        Gson g = new GsonBuilder().disableHtmlEscaping().create();
+
+
+//        String reu = StringEscapeUtils.escapeJson(b2);
+//        Log.i("kcc", reu);
+
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+//            Log.i("kcc", Html.escapeHtml(b2));
+//        }
+
+
+
+
+
+//
+//        String http = "http://www.www.baidu.com/sfsf?a=我的&b='\"";
+//
+//        Log.i("kcc",  Uri.encode(http));            //这个不会转单引号
+//        Log.i("kcc",  URLEncoder.encode(http));   //这个会转单引号
     }
 
 
--- app/src/main/java/com/kc/kproejct/ActivityFinder.java
+++ app/src/main/java/com/kc/kproejct/ActivityFinder.java
@@ -7,13 +7,15 @@ import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class ActivityFinder {
 
     private static final String MAIN_ACTIVITY_ACTION = "com.kc.life.rise";
 
-    public static List<ActivityInfo> getAllActivity(Context context) {
+    public static List<ActivityInfo> getAllActivity(final Context context) {
 
         PackageManager pm = context.getApplicationContext().getPackageManager();
         Intent intent = new Intent(MAIN_ACTIVITY_ACTION);
@@ -23,6 +25,14 @@ public class ActivityFinder {
         for(int i=infos.size() -1 ; i >=0 ; i--) {
             result.add(infos.get(i).activityInfo);
         }
+        Collections.sort(result, new Comparator<ActivityInfo>() {
+            @Override
+            public int compare(ActivityInfo lhs, ActivityInfo rhs) {
+                return rhs.loadLabel(context.getPackageManager()).toString().compareTo(
+                        lhs.loadLabel(context.getPackageManager()).toString()
+                );
+            }
+        });
         return result;
     }
 
